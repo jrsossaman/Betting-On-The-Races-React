d962ab8ae7fb7c1f889086df6b087308e94c6443
ChaseGreen98 <cgreen@basecampcodingacedemy.org>
2025-10-19 12:37:04 -0500

    fixed key name

--- src/components/dice_roller.jsx
+++ src/components/dice_roller.jsx
@@ -4,11 +4,11 @@ const DiceRoller = (driver1, driver2) => {
     var driver1Roll = (Math.floor(Math.random() * 20) + 1) + driver1.driveBonus;
     var driver2Roll = (MMath.floor(Math.random() * 20) + 1) + driver2.driveBonus;
     if (driver1Roll > driver2Roll) {
-        driver1.winning = true;
-        driver2.winning = false;
+        driver1.status = true;
+        driver2.status = false;
     } else if (driver2Roll > driver1Roll) {
-        driver2.winning = true;
-        driver1.winning = false;
+        driver2.status = true;
+        driver1.status = false;
     }
 };
 
